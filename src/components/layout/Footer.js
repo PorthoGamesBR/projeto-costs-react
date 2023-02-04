07a73455463c29163dda42df56085243b0031f35
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-04 11:29:05 -0300

    Adicionado Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,19 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+ 
+import styles from "./Footer.module.css"
 function Footer()
 {
     return (
-    <p>Footer</p>
+    <footer className={styles.footer}>
+        <ul className={styles.social_list}>
+            <li><FaFacebook /></li>
+            <li><FaInstagram /></li>
+            <li><FaLinkedin /></li>
+        </ul>
+        <p className={styles.copy_right}>
+            <span>Costs</span> &copy; 2021
+        </p>
+    </footer>
     )
 }
 
