f8f600921410cdf5106f5779b64efe9d403e1b4b
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-05 09:11:00 -0300

    Criado new project e projectForm

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,13 @@
+import ProjectForm from "../project/ProjectForm"
+import styles from "./NewProject.module.css"
+
 function NewProject() {
     return (
-    <h1>New Project</h1>
+    <div className={styles.newproject_container}>
+        <h1>Criar Projeto</h1>
+        <p>Crie seu projeto para depois adicionar serviços</p>
+        <ProjectForm />
+    </div>
     )
 }
 
