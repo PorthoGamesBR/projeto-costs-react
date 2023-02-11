bf7eb750d34e5e180cd8455e9f563218f8a71d0e
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-11 20:00:07 -0300

    Adicionando projetos via POST

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,30 @@
+import { useNavigate } from 'react-router-dom'
+
 import ProjectForm from "../project/ProjectForm"
 import styles from "./NewProject.module.css"
 
 function NewProject() {
+    const navigate = useNavigate()
+
+    function createPost(project) {
+
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http:/localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((err) => console.log(err))
+
+    }
+
     return (
     <div className={styles.newproject_container}>
         <h1>Criar Projeto</h1>
