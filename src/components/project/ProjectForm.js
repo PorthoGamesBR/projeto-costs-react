3bc69dcdaeef0404ac9dc814c7265cc5efde79c7
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-11 19:44:29 -0300

    Pega dados da database no project form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import {useState} from 'react'
+
 import Input from "../form/Input"
 import Select from "../form/Select"
 import SubmitButton from "../form/SubmitButton"
+
 import styles from "./ProjectForm.module.css"
 
 function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([])
+
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch((err) => console.log(err))
+
     return (
     <form className={styles.form}>
         <Input
