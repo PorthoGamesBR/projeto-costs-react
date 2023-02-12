3235289cfb59c3c68f236cf275ecf6c89c5c43ab
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-12 10:20:07 -0300

    Criado componente de mensagem e estilo basico

---
+++ src/components/layout/Message.js
@@ -0,0 +1,11 @@
+import styles from "./Message.module.css"
+
+function Message( {type, msg }) {
+    return (
+    <div className={`${styles.message}  ${styles[type]}` }>
+        <p>{msg}</p>
+    </div>
+    )
+}
+
+export default Message
