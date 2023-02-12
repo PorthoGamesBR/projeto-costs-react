f139cf365f261f5ac17ed099d6e654e93f3a7d69
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-12 11:39:51 -0300

    Pegando projetos da database

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,7 @@
 import { useLocation } from 'react-router-dom'
 
+import { useState, useEffect } from 'react'
+
 import Message from '../layout/Message'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
@@ -7,12 +9,30 @@ import LinkButton from '../layout/LinkButton'
 import styles from './Projects.module.css'
 
 function Projects() {
+    const [projects, setProjects] = useState([])
+
 
     const location = useLocation()
     let message = ''
     if(location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    },[])
 
     return (
     <div className={styles.project_container}>
