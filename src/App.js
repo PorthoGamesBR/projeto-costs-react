2111b1985267e845a9943e3f40d36aa6cb4c8eab
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-12 15:19:46 -0300

    Criada página de projeto individual

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 
@@ -20,7 +21,8 @@ function App() {
         <Route exact path="/projects" element={<Projects/>} />
         <Route exact path="/company" element={<Company />} />
         <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/newproject" element={<NewProject />} />   
+        <Route exact path="/newproject" element={<NewProject />} />  
+        <Route exact path="/project/:id" element={<Project />} />  
     </Routes>
     </Container>
       <Footer />
