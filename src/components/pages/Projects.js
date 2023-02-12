68892e36369e148dccc05464368e0361fc41e205
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-12 14:48:38 -0300

    Mudanca na funcao removeProject

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -44,10 +44,10 @@ function removeProject(id){
     fetch(`http://localhost:5000/projects/${id}`, {
     method: 'DELETE',
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
     },
-    }).then(resp => resp.json())
-    .then(data => {
+    }).then((resp) => resp.json())
+    .then(() => {
         setProjects(projects.filter((project) => project.id !== id))
         setProjectMessage('Projeto removido com sucesso!')
         // message
