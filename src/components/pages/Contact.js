dafa4f8f742ae0908d278443a3b5b3df9a263c13
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-02-15 22:36:24 -0300

    Começando pagina de contato

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,22 @@
+import styles from "Contact.module.css"
+import Container from "../layout/Container"
+
 function Contact() {
     return (
-    <h1>Contact</h1>
+    
+    <div className={styles.contact_container}>
+        <div className={styles.title_container}>
+            <h1>Contato</h1>
+            <p>Fale conosco</p>
+        </div>
+        <Container customClass="start">
+            // barra de icones de redes sociais
+
+            <div>
+                // formulario de contato
+            </div>
+        </Container>
+    </div>
     )
 }
 
